c0b9eabd1266e138c0fd68756263aeb4bd030256
GermanG55 <grmnrtr@gmail.com>
2022-09-02 12:45:51 -0500

    Corrección de la funcionalidad de multiplicacion

--- src/co/edu/unisabana/example/Principal.java
+++ src/co/edu/unisabana/example/Principal.java
@@ -1,9 +1,16 @@
 package co.edu.unisabana.example;
-
 public class Principal {
     public static void main(String[] args) {
         int EsPush = 4;
         int NoPull = 2;
+        int resultantSum = EsPush + NoPull;
+        System.out.println(resultantSum);
+        System.out.println("Hola mundo");
+        //jajasaludos
+        int resultantRest = EsPush - NoPull;
+        System.out.println(resultantRest);
+        int resultantDiv = EsPush / NoPull;
+        System.out.println(resultantDiv);
         int resultantMult = EsPush * NoPull;
         System.out.println(resultantMult);
 
