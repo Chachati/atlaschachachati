3c5df93101b26e46a139367be08a9617dca4e556
GermanG55 <grmnrtr@gmail.com>
2022-09-02 10:15:55 -0500

    jajasaludos

--- src/co/edu/unisabana/example/Principal.java
+++ src/co/edu/unisabana/example/Principal.java
@@ -4,6 +4,10 @@ public class Principal {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
         //jajasaludos
+        int EsPush = 1;
+        int NoPull = 2;
+        int result = EsPush - NoPull;
+        System.out.println(result);
         
     }
 }
