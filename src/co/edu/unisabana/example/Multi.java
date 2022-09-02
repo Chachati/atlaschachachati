bd986abf24b86b0e090dc8949f33580cf96c253e
Santiago Santos <santiago_sabad@hotmail.com>
2022-09-02 11:50:21 -0500

    Funcionalidad de raiz completa.

--- src/co/edu/unisabana/example/Multi.java
+++ src/co/edu/unisabana/example/Multi.java
@@ -2,6 +2,7 @@ package co.edu.unisabana.example;
 
 class Multijava {
     public static void main(String[] args) {
+        raiz();
         int EsPush = 1;
         int NoPull = 2;
         int resultant = EsPush * NoPull;
@@ -10,7 +11,16 @@ class Multijava {
     }
 
     public int suma(){
-        return 0;
 
+        return 0;
+    }
+
+    public static void raiz() {
+        int a = 5;
+        int b = 2;
+        int c =3 ;
+
+        System.out.println("la raiz cuadrada del numero " + Math.sqrt((a+b+c)*(a+b+c-a)*(a+b+c-b)));
     }
 }
+
