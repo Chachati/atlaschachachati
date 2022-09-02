04ad7414d31515ab19ef0c4ddf7d71a54a730fc3
Miguel Chachati <miguelchra@unisabana.edu.co>
2022-09-02 11:33:58 -0500

    Corrección de la funcionalidad de división

--- src/co/edu/unisabana/example/Principal.java
+++ src/co/edu/unisabana/example/Principal.java
@@ -2,9 +2,10 @@ package co.edu.unisabana.example;
 
 public class Principal {
     public static void main(String[] args) {
-        int EsPush = 1;
+        int EsPush = 2;
         int NoPull = 2;
         int resultant = EsPush / NoPull;
         System.out.println(resultant);
+
     }
 }
