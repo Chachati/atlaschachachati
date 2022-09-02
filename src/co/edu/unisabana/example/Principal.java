6ec73a3817f07a3cf88adec99a75285c39e10527
Miguel Chachati <miguelchra@unisabana.edu.co>
2022-09-02 10:36:41 -0500

    Creación de la funcionalidad de división

--- src/co/edu/unisabana/example/Principal.java
+++ src/co/edu/unisabana/example/Principal.java
@@ -2,9 +2,6 @@ package co.edu.unisabana.example;
 
 public class Principal {
     public static void main(String[] args) {
-        int EsPush = 2;
-        int NoPull = 2;
-        int resultant = EsPush / NoPull;
-        System.out.println(resultant);
+        System.out.println("Gato");
     }
 }
